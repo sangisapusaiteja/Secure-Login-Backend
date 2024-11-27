cc6d0774088162c33d7efeb9a390b345cccdbe05
tejasai38409@gmail.com <tejasai38409@gmail.com>
2024-11-27 22:48:23 +0530

    setup is done and fe part is mostly done

---
+++ build/static/js/257.bc8f6736.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[257],{257:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=257.bc8f6736.chunk.js.map
--- build/static/js/775.c50fbd89.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[775],{775:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=775.c50fbd89.chunk.js.map
